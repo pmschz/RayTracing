69c3f65597fed65198319172df7ef76a85ffaecc
pmschz <paulinasanchez177@gmail.com>
2025-03-22 01:59:46 -0500

    all files added

---
+++ build/CMakeFiles/image_gen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_gen.dir/main.cc.o"
+  "CMakeFiles/image_gen.dir/main.cc.o.d"
+  "image_gen"
+  "image_gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
