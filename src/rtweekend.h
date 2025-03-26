e26a599b299f0fcb23ddd9bbd9b300e23c800c54
pmschz <paulinasanchez177@gmail.com>
2025-03-26 01:42:48 -0500

    first render of diffuse sphere

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -3,16 +3,16 @@
 
 #include <cmath>
 #include <iostream>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
-
+// Constants
 // C++ Std Usings
 
 using std::make_shared;
 using std::shared_ptr;
-
-// Constants
 
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
@@ -23,9 +23,20 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
 // Common Headers
 
 #include "color.h"
+#include "interval.h"
 #include "ray.h"
 #include "vec3.h"
 
