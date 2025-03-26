604d3104ac6a8c7a1ec2f0361449d7b0d4ff8e68
pmschz <paulinasanchez177@gmail.com>
2025-03-26 17:56:18 -0500

    shiny metal image generated.

--- src/camera.h
+++ src/camera.h
@@ -4,6 +4,7 @@
 #include "color.h" // Include color.h explicitly
 #include "vec3.h"  
 #include "hittable.h"
+#include "material.h"
 
 class camera {
   public:
@@ -95,8 +96,11 @@ color ray_color(const ray& r, int depth, const hittable& world) const {
         hit_record rec;
 
         if (world.hit(r, interval(0.001, infinity), rec)) {
-            vec3 direction = rec.normal + random_unit_vector();
-            return 0.1 * ray_color(ray(rec.p, direction), depth-1, world);
+            ray scattered;
+            color attenuation;
+            if (rec.mat->scatter(r, rec, attenuation, scattered))
+                return attenuation * ray_color(scattered, depth-1, world);
+            return color(0,0,0);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
